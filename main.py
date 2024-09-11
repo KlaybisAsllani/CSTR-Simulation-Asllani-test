c3b1344daea28d04b79cf1d5bbef6149bd2910ea
Klaybis Asllani <klaybisa@gmail.com>
2024-09-11 13:05:12 -0500

    Update main.py

--- main.py
+++ main.py
@@ -6,21 +6,17 @@ import pid_control
 def main():
     print("Running CSTR Simulation...")
     
-    # Step Response
     print("Simulating Step Response...")
     step_response.simulate_step_response()
     
-    # Ramp Response
     print("Simulating Ramp Response...")
     ramp_response.simulate_ramp_response()
     
-    # Sinusoidal Response
     print("Simulating Sinusoidal Response...")
     sinusoidal_response.simulate_sinusoidal_response()
-    
-    # PID Control
+
     print("Simulating PID Control...")
     pid_control.simulate_pid_control()
-
+    
 if __name__ == "__main__":
     main()
