5eceef30340296f3a97355ef39c82d8892557803
Klaybis Asllani <klaybisa@gmail.com>
2024-09-11 13:06:06 -0500

    Update pid_control.py

--- pid_control.py
+++ pid_control.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import control
 
 def simulate_pid_control():
-    # Define system parameters
+    # System parameters
     V = 1.0  # Reactor volume (m³)
     F = 0.5  # Volumetric flow rate (m³/s)
     k = 0.1  # Reaction rate constant (1/s)
@@ -11,11 +11,10 @@ def simulate_pid_control():
     # Time constant
     tau = V / (F + V * k)
 
-    # Transfer function numerator and denominator (C_A(s) / C_A0(s))
+    # For transfer function 
     num = [1]  # Numerator (gain = 1)
     den = [tau, 1]  # Denominator (tau s + 1)
 
-    # Create transfer function
     system = control.TransferFunction(num, den)
 
     # PID controller parameters
@@ -26,7 +25,6 @@ def simulate_pid_control():
     # Create PID controller
     pid = control.TransferFunction([Kd, Kp, Ki], [1, 0])
 
-    # Closed-loop system with PID controller
     closed_loop = control.feedback(pid * system, 1)
 
     # Simulate the step response of the closed-loop system
