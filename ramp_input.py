d68caadd453d4873e027c7aad29d60f007d880d5
Klaybis Asllani <klaybisa@gmail.com>
2024-09-22 19:59:13 -0500

    Inclusion of Ramp Response

---
+++ ramp_input.py
@@ -0,0 +1,26 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import signal
+
+def simulate_ramp_response():
+    V = 1.0
+    F = 0.5
+    k = 0.1
+    tau = V / (F + V * k)
+    num = [1]
+    den = [tau, 1]
+    system = signal.TransferFunction(num, den)
+    t = np.linspace(0, 100, 1000)
+    u = t
+    t, response, _ = signal.lsim(system, U=u, T=t)
+    plt.plot(t, response, label='System Response')
+    plt.plot(t, u, label='Ramp Input', linestyle='--')
+    plt.title('CSTR Ramp Response (Concentration)')
+    plt.xlabel('Time [s]')
+    plt.ylabel('Concentration')
+    plt.legend()
+    plt.grid(True)
+    plt.show()
+
+if __name__ == "__main__":
+    simulate_ramp_response()
