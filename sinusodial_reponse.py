d407dc8568e8fc655a75987af2130236777dd1ee
Klaybis Asllani <klaybisa@gmail.com>
2024-10-01 11:30:31 -0500

    Update sinusodial_reponse.py

--- sinusodial_reponse.py
+++ sinusodial_reponse.py
@@ -3,19 +3,16 @@ import matplotlib.pyplot as plt
 from scipy import signal
 
 def simulate_sinusoidal_response():
-    # Define system parameters
     V = 1.0  # Reactor volume (m³)
     F = 0.5  # Volumetric flow rate (m³/s)
     k = 0.1  # Reaction rate constant (1/s)
 
-    # Time constant
     tau = V / (F + V * k)
 
     # Transfer function numerator and denominator (C_A(s) / C_A0(s))
     num = [1]  # Numerator (gain = 1)
     den = [tau, 1]  # Denominator (tau s + 1)
 
-    # Create transfer function
     system = signal.TransferFunction(num, den)
 
     # Simulate response to a sinusoidal input
@@ -23,7 +20,6 @@ def simulate_sinusoidal_response():
     u = np.sin(t)  # Sinusoidal input
     t, response, _ = signal.lsim(system, U=u, T=t)
 
-    # Plot the result
     plt.plot(t, response)
     plt.title('CSTR Sinusoidal Response (Concentration)')
     plt.xlabel('Time [s]')
