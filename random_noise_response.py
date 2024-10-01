5ab1121ce431b42bc78717ab36523c8e4144a7fa
Klaybis Asllani <klaybisa@gmail.com>
2024-10-01 12:01:53 -0500

    Update random_noise_response.py

--- random_noise_response.py
+++ random_noise_response.py
@@ -2,24 +2,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy import signal
 
-def simulate_random_noise_response():
-    V = 1.0
-    F = 0.5
-    k = 0.1
-
+def simulate_random_noise_response(V, F, k, noise_mean, noise_std):
     tau = V / (F + V * k)
-
     num = [1]
     den = [tau, 1]
-
     system = signal.TransferFunction(num, den)
-
     t = np.linspace(0, 100, 1000)
     np.random.seed(0)
-    u = np.random.normal(0, 1, len(t))
-
+    u = np.random.normal(noise_mean, noise_std, len(t))
     t, response, _ = signal.lsim(system, U=u, T=t)
-
     plt.plot(t, response, label='System Response')
     plt.plot(t, u, label='Random Noise Input', linestyle='--')
     plt.title('CSTR Random Noise Response (Concentration)')
@@ -30,4 +21,4 @@ def simulate_random_noise_response():
     plt.show()
 
 if __name__ == "__main__":
-    simulate_random_noise_response()
+    simulate_random_noise_response(1.0, 0.5, 0.1, 0, 1)
