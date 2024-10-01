a1b1bd2bba7c617c0e75b1850e6ef1559ab879eb
Klaybis Asllani <klaybisa@gmail.com>
2024-10-01 11:59:07 -0500

    Update step_response.py

--- step_response.py
+++ step_response.py
@@ -2,21 +2,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy import signal
 
-def simulate_step_response():
-    V = 1.0  # Reactor volume (m³)
-    F = 0.5  # Volumetric flow rate (m³/s)
-    k = 0.1  # Reaction rate constant (1/s)
-
+def simulate_step_response(V, F, k):
     tau = V / (F + V * k)
-
-    # Transfer function numerator and denominator (C_A(s) / C_A0(s))
-    num = [1]  # Numerator (gain = 1)
-    den = [tau, 1]  # Denominator (tau s + 1)
-
+    num = [1]
+    den = [tau, 1]
     system = signal.TransferFunction(num, den)
-
     t, response = signal.step(system)
-
     plt.plot(t, response)
     plt.title('CSTR Step Response (Concentration)')
     plt.xlabel('Time [s]')
@@ -25,4 +16,4 @@ def simulate_step_response():
     plt.show()
 
 if __name__ == "__main__":
-    simulate_step_response()
+    simulate_step_response(1.0, 0.5, 0.1)
